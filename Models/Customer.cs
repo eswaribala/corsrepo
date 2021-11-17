971a88ea5711af08b83cb1dc3d6a7706d0998bfe
eswaribala <parameswaribala@gmail.com>
2021-11-17 14:35:20 +0530

    updated

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CQRSDemo.Models
+{
+    public class Customer
+    {
+        public long CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public long MobileNo { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Invoice> colllection { get; set; }
+
+    }
+}
---
+++ Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CQRSDemo.Models
+{
+    public class Invoice
+    {
+
+        public long InvoiceNo { get; set; }
+        public DateTime DOI { get; set; }
+
+        public long Amount { get; set; }
+
+    }
+}
