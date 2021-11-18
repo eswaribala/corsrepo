0650e46852161b906ead17292c519898e13424d1
eswaribala <parameswaribala@gmail.com>
2021-11-18 08:21:39 +0530

    updated

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,12 @@
+using CQRSDemo.Commands;
+using CQRSDemo.Events;
+using CQRSDemo.Models.Mongo;
+using CQRSDemo.Models.Sqlite;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,11 +37,27 @@ namespace CQRSDemo
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CQRSDemo", Version = "v1" });
             });
+            services.AddDbContext<CustomerSQLiteDatabaseContext>
+               (options => options.UseSqlite
+               (Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<CustomerSQLiteRepository>();
+            services.AddTransient<CustomerMongoRepository>();
+            services.AddTransient<AMQPEventPublisher>();
+            services.AddSingleton<CustomerMessageListener>();
+            services.AddScoped<ICommandHandler<Command>, CustomerCommandHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService
+               <IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.
+                    GetRequiredService<CustomerSQLiteDatabaseContext>();
+                context.Database.EnsureCreated();
+            }
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
